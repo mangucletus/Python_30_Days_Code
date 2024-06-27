447f93758dde276f3ec998b1405b1d09c8bacd5a
mangucletus <mangucletus@gmail.com>
2024-06-27 20:26:39 +0000

    day 1

---
+++ 1_day_introduction/helloworld.py
@@ -0,0 +1,36 @@
+# Day1 of 30 days of python coding
+# Printing to the console
+
+print("hello world")
+
+print(8+5)   #addition(+)
+print(9-3)   #subtraction(-)
+print(2*7)   #multiplication(*)
+print(6/2)   #division(/)
+print(4**2)  #exponential(**)
+print(8%3)   #modulos (%)
+print(3//2)  #floor division operator(//)
+
+# checking data types
+
+data1 = 19
+print(type(data1)) #integer
+data2 = 5.23
+print(type(data2)) #float
+
+data22 = 3.234545
+print(type(data22))
+data3 = 2+3j
+print(type(data3)) #complex
+data4 = "How are you"
+print(type(data4))  #string
+data5 = [1,3,5,7,9]
+print(type(data5))  #list
+
+mydict = {'name': 'Cletus'}
+print(type(mydict)) #dictionary
+
+myset = {2.34, 45.2, 12.3}
+print(type(myset))  #set
+mytuple = (23, 45, 34, 68)
+print(type(mytuple))  #tuple
